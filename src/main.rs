887b541ae517d17545c0737381f9641c38a7a752
orseti <hello@orseti.com>
2021-09-15 22:49:34 +0100

    more refactor, now only `kmain` in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,25 @@
 mod core_fns;
 #[doc(hidden)]
 mod efi;
-#[doc(hidden)]
-#[macro_use]
-mod print;
-pub mod kernel;
+pub mod serial;
 
 use efi::{EfiHandle, EfiStatus, EfiSystemTable};
 
-#[no_mangle]
-extern "C" fn efi_main(_handle: EfiHandle, st: *mut EfiSystemTable) -> EfiStatus {
-    unsafe {
-        efi::register_system_table(st);
-        println!("Loaded UEFI System Table at {:p}", st);
 
-        println!("Exiting Boot services...");
-        efi::exit_boot_services(_handle);
-    }
+pub fn kmain() {
 
 
-
-    panic!("efi_main exited");
+    panic!("reached end of kmain()");
 }
 
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    println!("!!! PANIC !!!");
+    /*println!("!!! PANIC !!!");
     if let Some(loc) = _info.location() {
         println!("Location: {}", loc);
     }
     if let Some(msg) = _info.message() {
         println!("Message:  {}", msg);
-    }
+    }*/
     loop {}
 }
