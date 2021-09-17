40a8d27305589bbcf7297f5328985fa05a0d7fa3
orseti <hello@orseti.com>
2021-09-17 01:14:21 +0100

    Add modules from prev. commits and start printing in `panic_handler` again

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,34 @@
 #![no_main]
 #![feature(asm, abi_efiapi, llvm_asm, bool_to_option)]
 #![feature(panic_info_message)]
+pub mod acpi;
 #[doc(hidden)]
 mod core_fns;
+pub mod cpu;
 #[doc(hidden)]
 mod efi;
-pub mod serial;
-pub mod acpi;
+mod error;
 pub mod mm;
-pub mod cpu;
 pub mod register;
+pub mod serial;
 
 use efi::{EfiHandle, EfiStatus, EfiSystemTable};
 
-
 pub fn kmain() {
     dump_state!();
-    
+
     panic!("reached end of kmain()");
 }
 
+
 #[panic_handler]
 fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    /*println!("!!! PANIC !!!");
+    println!("!!! PANIC !!!");
     if let Some(loc) = _info.location() {
         println!("Location: {}", loc);
     }
     if let Some(msg) = _info.message() {
         println!("Message:  {}", msg);
-    }*/
+    }
     loop {}
 }
