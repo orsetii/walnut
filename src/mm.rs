8e232a8a5d915a3a2a5dfbabade1edc386cbd2c0
orseti <hello@orseti.com>
2021-09-17 01:13:16 +0100

    Small changes to `mm.rs`

--- src/mm.rs
+++ src/mm.rs
@@ -1,16 +1,15 @@
 //! Various memory management and CPU state mangement functions and macros.
 
-
 /// Represents a physical address
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct PhysAddr(pub usize);
 
 /// Reads `T` at physical address `paddr`
 pub unsafe fn read_physaddr<T>(paddr: PhysAddr) -> T {
-    core::ptr::read_volatile(paddr.0 as *mut T)
+    core::ptr::read(paddr.0 as *mut T)
 }
 
 /// Writes `val` at physical address `paddr`
 pub unsafe fn write_physaddr<T>(paddr: PhysAddr, val: T) {
-    core::ptr::write_volatile(paddr.0 as *mut T, val)
+    core::ptr::write(paddr.0 as *mut T, val)
 }
-
