10acc58f8289900404fdc19cd4bb2f53796fca3d
orseti <hello@orseti.com>
2021-09-19 01:58:29 +0100

    Add rest of APIC record structs for `MADT` and `match` them all properly

--- src/acpi/structures/apic.rs
+++ src/acpi/structures/apic.rs
@@ -1,9 +1,53 @@
+#[derive(Debug, Copy, Clone)]
+#[repr(u8)]
+pub enum ApicRecordType {
+    ProcessorLocalApic               = 0,
+    IoApic                           = 1,
+    IoApicInterruptSourceOverride    = 2,
+    IoApicNonMaskableInterruptSource = 3,
+    LocalApicNonMaskableInterrupts   = 4,
+    LocalApicAddressOverride         = 5,
+    ProcessorLocalX2Apic             = 9,
+    Unknown(u8),
+}
+
+impl From<u8> for ApicRecordType {
+    fn from(v: u8) -> Self {
+        match &v {
+            0 => Self::ProcessorLocalApic,
+            1 => Self::IoApic,
+            2 => Self::IoApicInterruptSourceOverride,
+            3 => Self::IoApicNonMaskableInterruptSource,
+            4 => Self::LocalApicNonMaskableInterrupts,
+            5 => Self::LocalApicAddressOverride,
+            9 => Self::ProcessorLocalX2Apic,
+            _ => Self::Unknown(v),
+        }
+    }
+}
+
+
+bitflags! {
+    struct OtherApicFlags: u16 {
+        /// If set then the interrupt is active when low
+        const ACTIVE_WHEN_LOW = 1 << 1;
+        /// If set then interrupt is level-triggered
+        const LEVEL_TRIGGERED_INTERRUPT = 1 << 7;
+    }
+}
+
 
 /// We should get one of these for each core
+/// This type represents a I/O APIC. 
+/// The global system interrupt base is the first interrupt 
+/// number that this I/O APIC handles. 
+/// You can see how many interrupts it handles using 
+/// the register by getting the number of redirection 
+/// entries from register 0x01
 #[derive(Debug, Copy, Clone)]
 #[repr(C, packed)]
-pub struct LocalApic {
-    acpi_processor_uid: u8,
+pub struct ProcessorLocalApic {
+    acpi_proc_id: u8,
     apic_id: u8,
     flags: LocalApicFlags,
 }
@@ -15,6 +59,55 @@ bitflags! {
         const ONLINE_CAPABLE = 1 << 1;
     }
 }
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, packed)]
+pub struct IoApic {
+    io_apic_id: u8,
+    reserved: u8,
+    io_apic_addr: u32,
+    global_system_interrupt_base: u32,
+}
+
+/// This entry type contains the data for an Interrupt Source Override. This explains how IRQ sources are mapped to global system interrupts. 
+#[derive(Debug, Copy, Clone)]
+#[repr(C, packed)]
+pub struct IoApicInterruptSourceOverride {
+    bus_source: u8,
+    irq_source: u8,
+    global_system_interrupt: u32,
+    flags: OtherApicFlags,
+}
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, packed)]
+pub struct IoApicNonMaskableInterruptSource {
+    nmi_source: u8,
+    reserved: u8,
+    flags: OtherApicFlags,
+    global_system_interrupt: u32,
+}
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, packed)]
+pub struct LocalApicNonMaskableInterrupts {
+    /// ACPI Processor ID (0xFF means all processors)
+    acpi_proc_id: u8,
+    flags: OtherApicFlags,
+    /// Should be configured with the `LINT0` and `LINT1` entries in the 
+    /// local vector table of the relevant processors local APIC.
+    lint: u8,
+}
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, packed)]
+pub struct LocalApicAddressOverride {
+    /// ACPI Processor ID (0xFF means all processors)
+    reserved: u16,
+    /// 64-bit physical address of Local APIC
+    local_apic_physaddr: u64,
+}
+
 #[derive(Debug, Copy, Clone)]
 #[repr(C, packed)]
 pub struct LocalX2Apic {
@@ -24,6 +117,5 @@ pub struct LocalX2Apic {
     x2apic_id: u32,
     /// same as Local APIC Flags
     flags: LocalApicFlags,
-
-    acpi_proc_id: u32,
+    acpi_id: u32,
 }
--- src/acpi/structures/madt.rs
+++ src/acpi/structures/madt.rs
@@ -1,13 +1,14 @@
-use super::apic::{LocalApic, LocalX2Apic};
-use super::{Table, TableType, Error, Result};
-use crate::mm::{self, PhysAddr};
+use super::apic::*;
+use super::{Error, Result, TableType};
+use crate::acpi::structures::apic::ApicRecordType;
 use crate::efi_println;
+use crate::mm::{self, PhysAddr};
 use core::mem::size_of;
-
 
 #[repr(C, packed)]
 pub struct Madt {
-    table: Table,
+    local_apic_addr: u32,
+    flags: u32, // TODO add bitflags
 }
 impl Madt {
     pub unsafe fn from_addr(addr: PhysAddr, size: usize) -> Result<Self> {
@@ -18,7 +19,11 @@ impl Madt {
         let local_apic_addr = slice.consume::<u32>().map_err(|_| E)?;
 
         // Get APIC flags
-        let local_apic_flags = slice.consume::<u32>().map_err(|_| E)?;
+        let flags = slice.consume::<u32>().map_err(|_| E)?;
+        let ret = Self {
+            local_apic_addr,
+            flags,
+        };
 
         // Handle Interrupt Contoller Structures
         while slice.len() > 0 {
@@ -32,18 +37,69 @@ impl Madt {
 
             efi_println!("{:#x} {}", typ, len);
 
-            match typ {
-                0 => {
+            // Could probably be done better with
+            // a generic and bindings for size functions?
+            // ah well!
+            match ApicRecordType::from(typ) {
+                ApicRecordType::ProcessorLocalApic => {
                     // Ensure data is correct size
-                    if len as usize != size_of::<LocalApic>() {
+                    if len as usize != size_of::<ProcessorLocalApic>() {
                         return Err(E);
                     }
 
-                    let apic = slice.consume::<LocalApic>().map_err(|_| E)?;
+                    let apic = slice.consume::<ProcessorLocalApic>().map_err(|_| E)?;
                     efi_println!("{:#x?}", apic);
                 }
-                9 => {
-                    
+                ApicRecordType::IoApic => {
+                    // Ensure data is correct size
+                    if len as usize != size_of::<IoApic>() {
+                        return Err(E);
+                    }
+
+                    let apic = slice.consume::<IoApic>().map_err(|_| E)?;
+                    efi_println!("{:#x?}", apic);
+                }
+                ApicRecordType::IoApicInterruptSourceOverride => {
+                    // Ensure data is correct size
+                    if len as usize != size_of::<IoApicInterruptSourceOverride>() {
+                        return Err(E);
+                    }
+
+                    let apic = slice
+                        .consume::<IoApicInterruptSourceOverride>()
+                        .map_err(|_| E)?;
+                    efi_println!("{:#x?}", apic);
+                }
+                ApicRecordType::IoApicNonMaskableInterruptSource => {
+                    // Ensure data is correct size
+                    if len as usize != size_of::<IoApicNonMaskableInterruptSource>() {
+                        return Err(E);
+                    }
+
+                    let apic = slice.consume::<ProcessorLocalApic>().map_err(|_| E)?;
+                    efi_println!("{:#x?}", apic);
+                }
+                ApicRecordType::LocalApicNonMaskableInterrupts => {
+                    // Ensure data is correct size
+                    if len as usize != size_of::<LocalApicNonMaskableInterrupts>() {
+                        return Err(E);
+                    }
+
+                    let apic = slice
+                        .consume::<LocalApicNonMaskableInterrupts>()
+                        .map_err(|_| E)?;
+                    efi_println!("{:#x?}", apic);
+                }
+                ApicRecordType::LocalApicAddressOverride => {
+                    // Ensure data is correct size
+                    if len as usize != size_of::<LocalApicAddressOverride>() {
+                        return Err(E);
+                    }
+
+                    let apic = slice.consume::<LocalApicAddressOverride>().map_err(|_| E)?;
+                    efi_println!("{:#x?}", apic);
+                }
+                ApicRecordType::ProcessorLocalX2Apic => {
                     // Ensure data is correct size
                     if len as usize != size_of::<LocalX2Apic>() {
                         return Err(E);
@@ -60,6 +116,6 @@ impl Madt {
 
         efi_println!("{:#x}", local_apic_addr);
 
-        panic!();
+        Ok(ret)
     }
 }
