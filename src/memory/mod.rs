3b81f9ac84ba0911b1f22f2accffcbfe475a3c44
orseti <hello@orseti.com>
2021-09-20 17:58:51 +0100

    Add function signatures for init functions for `memory` module

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,3 +1,4 @@
+pub mod paging;
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct MemoryRange {
@@ -23,3 +24,40 @@ pub struct PhysAddr(pub u64);
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 #[repr(transparent)]
 pub struct VirtAddr(pub u64);
+
+/// Align address downwards.
+///
+/// Returns the greatest x with alignment `align` so that x <= addr. The alignment must be
+///  a power of 2.
+#[inline]
+pub fn align_down(addr: u64, align: u64) -> u64 {
+    assert!(align.is_power_of_two(), "`align` must be a power of two");
+    addr & !(align - 1)
+}
+
+/// Align address upwards.
+///
+/// Returns the smallest x with alignment `align` so that x >= addr. The alignment must be
+/// a power of 2.
+#[inline]
+pub fn align_up(addr: u64, align: u64) -> u64 {
+    assert!(align.is_power_of_two(), "`align` must be a power of two");
+    let align_mask = align - 1;
+    if addr & align_mask == 0 {
+        addr // already aligned
+    } else {
+        (addr | align_mask) + 1
+    }
+}
+
+/// Initializes memory structures and allocators, linearly in the form:
+/// ```text
+/// Re-Identity Map Memory via UEFI
+///             🠗
+/// Intialize Page Frame Allocator
+///             🠗
+/// Intialize Heap Allocator
+/// ```
+pub fn init() {
+
+}
