ea16a8317a1aca60e4bcbde4250fa1dbd880b2c4
orseti <hello@orseti.com>
2021-09-20 09:12:47 +0100

    QEMU module changes, introduce `custom_port` feature

---
+++ shared/qemu/README.md
@@ -0,0 +1,6 @@
+# QEMU Library for OSDev
+
+To enable custom QEMU exit IO port addresses in the function signatures add
+```toml
+qemu = { version = "*", features = ["custom_port"]}
+```
--- shared/qemu/src/lib.rs
+++ shared/qemu/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![feature(asm)]
 
+
 const QEMU_EXIT_IO_PORT: u16 = 0xF4;
 
 
@@ -15,15 +16,58 @@ pub enum QemuExitCode {
 /// Exits qemu with `exit_code` via writing to the port address for
 /// the `isa-debug-exit` device, which must be set in the QEMU launch flags.
 /// If we fail to exit, a panic occurs.
-pub fn exit_qemu(exit_code: QemuExitCode, port_addr: Option<u16>) {
+#[cfg(feature = "custom_port")]
+pub fn exit(exit_code: QemuExitCode, port_addr: u16) {
     unsafe {
         // Write to the provided port or the default if not provided
-        write_to_port(port_addr.unwrap_or(QEMU_EXIT_IO_PORT), exit_code as u32)
+        write_to_port(port_addr, exit_code as u32)
     }
-    panic!("QEMU did not exit after `exit_qemu` call
-           Please add the `isa-debug-exit` device to your QEMU flags");
+    unreachable!(
+        "QEMU did not exit after `exit_qemu` call
+         Please add the `isa-debug-exit` device to your QEMU flags"
+         );
 }
 
+#[cfg(feature = "custom_port")]
+#[inline]
+pub fn exit_success(port_addr: u16) {
+    exit(QemuExitCode::Success, port_addr)
+}
+
+#[cfg(feature = "custom_port")]
+#[inline]
+pub fn exit_failed(port_addr: u16) {
+    exit(QemuExitCode::Failed, port_addr)
+}
+
+
+
+/// Exits qemu with `exit_code` via writing to the port address for
+/// the `isa-debug-exit` device, which must be set in the QEMU launch flags.
+/// If we fail to exit, a panic occurs.
+#[cfg(not(feature = "custom_port"))]
+pub fn exit(exit_code: QemuExitCode) {
+    unsafe {
+        // Write to the provided port or the default if not provided
+        write_to_port(QEMU_EXIT_IO_PORT, exit_code as u32)
+    }
+    unreachable!(
+        "QEMU did not exit after `exit_qemu` call
+         Please add the `isa-debug-exit` device to your QEMU flags"
+         );
+}
+
+#[cfg(not(feature = "custom_port"))]
+#[inline]
+pub fn exit_success() {
+    exit(QemuExitCode::Success)
+}
+
+#[cfg(not(feature = "custom_port"))]
+#[inline]
+pub fn exit_failed() {
+    exit(QemuExitCode::Failed)
+}
 
 unsafe fn write_to_port(port: u16, val: u32) {
     asm!("out dx, eax", in("dx") port, in("eax") val, options(nomem, nostack, preserves_flags));
