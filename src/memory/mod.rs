c683b935abfbcf6d807032d5793630c974d2d4a4
orseti <hello@orseti.com>
2021-09-21 23:20:57 +0100

    Move `PhysAddr`, `VirtAddr` out of `memory/mod.rs` and into seperate sub modules

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,54 +1,23 @@
+pub mod frame;
 pub mod paging;
+pub mod utils;
 
-#[derive(Clone, Copy, Debug, Default)]
-pub struct MemoryRange {
-    pub start: u64,
-    pub end:   u64,
-}
+pub use utils::{
+    addr::{Addr, PhysAddr, VirtAddr},
+    align_down, align_up,
+    rangeset::{Range, RangeSet},
+    Error, Result,
+};
 
-impl MemoryRange {
-    pub fn new() -> Self {
-        MemoryRange { start: 0, end: 0 }
-    }
-    pub fn size(&self) -> u64 {
-        self.end - self.start
-    }
-}
-
-/// Represents a physical address
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
-#[repr(transparent)]
-pub struct PhysAddr(pub u64);
-
-/// Represents a physical address
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
-#[repr(transparent)]
-pub struct VirtAddr(pub u64);
-
-/// Align address downwards.
-///
-/// Returns the greatest x with alignment `align` so that x <= addr. The alignment must be
-///  a power of 2.
-#[inline]
-pub fn align_down(addr: u64, align: u64) -> u64 {
-    assert!(align.is_power_of_two(), "`align` must be a power of two");
-    addr & !(align - 1)
-}
-
-/// Align address upwards.
-///
-/// Returns the smallest x with alignment `align` so that x >= addr. The alignment must be
-/// a power of 2.
-#[inline]
-pub fn align_up(addr: u64, align: u64) -> u64 {
-    assert!(align.is_power_of_two(), "`align` must be a power of two");
-    let align_mask = align - 1;
-    if addr & align_mask == 0 {
-        addr // already aligned
-    } else {
-        (addr | align_mask) + 1
+pub fn init_heap_allocator(memory_range: Range) {
+    unsafe {
+        paging::ALLOCATOR
+            .lock()
+            .init(memory_range.start, memory_range.end);
     }
 }
+
+pub fn init_frame_allocator() {}
 
 /// Initializes memory structures and allocators, linearly in the form:
 /// ```text
@@ -58,11 +27,4 @@ pub fn align_up(addr: u64, align: u64) -> u64 {
 ///             🠗
 /// Intialize Heap Allocator
 /// ```
-pub fn init() {
-
-}
-
-#[alloc_error_handler]
-fn alloc_error_handler(layout: core::alloc::Layout) -> ! {
-    panic!("Allocator Error {:#x?}", layout);
-}
+pub fn init() {}
