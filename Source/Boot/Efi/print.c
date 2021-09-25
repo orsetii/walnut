5c251c7e110eefd44b5e8301d4fbd9dd1a177b21
orseti <hello@orseti.com>
2021-09-25 15:00:59 +0100

    `UEFI` print routines

---
+++ Source/Boot/Efi/print.c
@@ -0,0 +1,20 @@
+/*#include "efi.h"
+
+
+// Print routines for 16-bit characters
+
+EFI_STATUS wprint(str16 out_string)
+{
+    return ST->ConOut->OutputString(ST->ConOut, out_string);
+}
+
+EFI_STATUS wprintln(str16 out_string)
+{
+    // TODO
+    return ST->ConOut->OutputString(ST->ConOut, out_string);
+}
+
+
+// Print routines for 8-bit characters
+// TODO
+*/
