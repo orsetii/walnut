0c660d6c207dfe304f8336978be39c82f33b4bba
orseti <hello@orseti.com>
2021-09-25 15:00:06 +0100

    move `hello.c` to `main.c`

--- Source/main.c
+++ Source/main.c
@@ -1,4 +1,36 @@
+//#include "efi.h"
+
+ 
+ /*
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+{
+	// Set the global EFI_SYSTEM_TABLE
+	ST = SystemTable;
+	(void)ImageHandle;
+
+	EFI_STATUS Status;
+	EFI_INPUT_KEY Key;
+ 
+	// Print message
+	wprint(L"Welcome to Walnut OS\n\r");
+	
+ 
+	// Empty the console input buffer to flush out any keystrokes entered before this point.
+	Status = SystemTable->ConIn->Reset(SystemTable->ConIn, false);
+	if(EFI_ERROR(Status)) return Status;
+ 
+	// Wait for keypress. 
+	while((Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY) ;
+
+	while(1) { asm("pause");}
+ 
+	return Status;
+}
+*/
+
 #include "efi.h"
+
+
  
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
 {
@@ -21,3 +53,4 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
  
 	return Status;
 }
+
