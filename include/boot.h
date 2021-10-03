052ce6b8517845a0e1ea693ca4a54fbbe24d8b63
orseti <hello@orseti.com>
2021-10-03 20:04:36 +0100

    removed C stuff

--- include/boot.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "types.h"
-#include "print.h"
-#include "boot/graphics/framebuffer.h"
-frame_buffer_t* init_gop();
-
-extern void exit_qemu(void);
--- include/macros.h
+++
@@ -1,2 +0,0 @@
-#include "macros/generic.h"
-#include "macros/efi.h"
