3ff129bfb5f628753c2445b168d4cbcd4b09ae47
Tom French <hello@orseti.com>
2024-02-22 11:44:33 +0000

    misc kmain changes

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,15 +1,22 @@
 #include "common.h"
+#include "io.h"
 #include "mem.h"
 #include "print.h"
 #include "serial.h"
 
-void kmain() {
+void kmain()
+{
+    kprintf("Entered main at: %p", kmain);
+    initialize_uart();
 
     init_ptable();
 
+    // kprintf("\nFound %x at 0x801f9008\n", *(u64*)0x801f9000);
 
-  kprintf("Entered main at: %p", kmain);
-  while (1)
-  {
-  }
+    // mmio_writeb(mtimecmp, 0, mmio_readb(mtime, 0) + 10000000);
+    //*(volatile char*)0x0 = 1;
+
+    kprintf("Main Loop started...");
+    while (1) {
+    }
 }
