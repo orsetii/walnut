444ab055659eba987cafdf23ce646b7654cf2591
Tom French <hello@orseti.com>
2024-02-22 11:44:05 +0000

    formatting changes

--- src/kernel/mem/addr.c
+++ src/kernel/mem/addr.c
@@ -1,25 +1,24 @@
 #include "mem/addr.h"
 
-paddr translate_virtual_address(vaddr va)
-{
-    int vpn[3] = {(va.fields.vpn2 >> 27) & 0x1FF, (va.fields.vpn1 >> 18) & 0x1FF, (va.fields.vpn0 >> 9) & 0x1FF};
-    ptable *current_table = get_root_page_table();
+paddr translate_virtual_address(vaddr va) {
+  int vpn[3] = {(va.fields.vpn2 >> 27) & 0x1FF, (va.fields.vpn1 >> 18) & 0x1FF,
+                (va.fields.vpn0 >> 9) & 0x1FF};
 
-    for (int i = 2; i >= 0; i--)
-    {
-        ptable_entry pte = current_table->entries[vpn[i]];
-        if (!is_valid(&pte))
-            return PADDR(NULL); // Page fault
+  ptable *current_table = get_root_page_table();
 
-        if (is_leaf(&pte))
-        {
-            return PADDR((pte.ppn << 12) | (va.raw & 0xFFF)); // Construct physical address
-        }
-        else
-        {
-            current_table = (ptable *)((size_t)pte.ppn << 12); // Next page table level
-        }
+  for (int i = 2; i >= 0; i--) {
+    ptable_entry pte = current_table->entries[vpn[i]];
+    if (!is_valid(&pte))
+      return PADDR(NULL); // Page fault
+
+    if (is_leaf(&pte)) {
+      return PADDR((pte.ppn << 12) |
+                   (va.raw & 0xFFF)); // Construct physical address
+    } else {
+      current_table =
+          (ptable *)((size_t)pte.ppn << 12); // Next page table level
     }
+  }
 
-    return PADDR(NULL); // Page fault
+  return PADDR(NULL); // Page fault
 }
--- src/kernel/mem/page.c
+++ src/kernel/mem/page.c
@@ -4,34 +4,27 @@ const size_t PAGE_SIZE = 1 << 12;
 const size_t PAGE_ORDER = 12;
 static size_t ALLOC_START = 0;
 
-size_t get_num_pages(void)
-{
-  return NUM_OF_PAGES;
-}
+size_t get_num_pages(void) { return NUM_OF_PAGES; }
 
 // Align pointer to nearest 2^order bytes, rounded up
-size_t align_val(size_t val, size_t order)
-{
+size_t align_val(size_t val, size_t order) {
   size_t o = (1ull << order) - 1;
   return (val + o) & ~o;
 }
 
 // Get page address from page id
-static size_t page_address_from_id(size_t id)
-{
+static size_t page_address_from_id(size_t id) {
   return ALLOC_START + PAGE_SIZE * id;
 }
 
-void pgalloc_init()
-{
+void pgalloc_init() {
   // Clear all the page flags
   zero_bytes((void *)HEAP_START, NUM_OF_PAGES);
 
   ALLOC_START = align_val(HEAP_START + NUM_OF_PAGES, PAGE_ORDER);
 }
 
-void *pgalloc_zalloc(uint pages)
-{
+void *pgalloc_zalloc(uint pages) {
   void *ptr = pgalloc_alloc(pages);
 
   if (ptr == NULL)
@@ -40,47 +33,37 @@ void *pgalloc_zalloc(uint pages)
   size_t sz = (PAGE_SIZE * pages) / 8;
   u64 *it = (u64 *)ptr;
 
-  while (it++ < (u64 *)(ptr + sz))
-  {
+  while (it++ < (u64 *)(ptr + sz)) {
     *it = 0;
   }
 
   return ptr;
 }
 
-void *pgalloc_alloc(uint pages)
-{
+void *pgalloc_alloc(uint pages) {
   page_list_flags *ptr = (page_list_flags *)HEAP_START;
   ASSERT(pages > 0, "Attempted to allocate 0 or less pages");
 
-  for (size_t i = 0; i < (NUM_OF_PAGES - pages); i++)
-  {
+  for (size_t i = 0; i < (NUM_OF_PAGES - pages); i++) {
     bool found = true;
 
-    if (!is_taken(ptr + i))
-    {
+    if (!is_taken(ptr + i)) {
       // Iterate from *i, search that we have `pages`
       // of contiguous pages
-      for (size_t j = i; j < (i + pages); j++)
-      {
+      for (size_t j = i; j < (i + pages); j++) {
 
-        if (is_taken(ptr + j))
-        {
+        if (is_taken(ptr + j)) {
           found = false;
           break;
         }
       }
-    }
-    else
-    {
+    } else {
       continue;
     }
 
-    if (found)
-    {
+    if (found) {
 
-      for (size_t pi = 0; pi <= pages - 1; pi++)
-      {
+      for (size_t pi = 0; pi <= pages - 1; pi++) {
         set_taken(ptr + pi);
       }
 
@@ -93,29 +76,25 @@ void *pgalloc_alloc(uint pages)
   return NULL;
 }
 
-void pgalloc_free(void *ptr)
-{
+void pgalloc_free(void *ptr) {
   ASSERT(ptr != NULL, "Attempted to free a NULL pointer");
 
   // Calculate the pagelistflags address
   page_list_flags *plf = get_plf_for_addr(ptr);
 
-  while (is_taken(plf) && !is_last(plf))
-  {
+  while (is_taken(plf) && !is_last(plf)) {
     set_empty(plf);
     plf++;
   }
 
   // Check and clear the last PLF for this allocation block
-  ASSERT(
-      is_last(plf),
-      "The last in-use PLF for this allocation did not have the 'LAST' flag set");
+  ASSERT(is_last(plf), "The last in-use PLF for this allocation did not have "
+                       "the 'LAST' flag set");
   set_empty(plf);
   set_notlast(plf);
 }
 
-page_list_flags *get_plf_for_addr(void *ptr)
-{
+page_list_flags *get_plf_for_addr(void *ptr) {
   return (page_list_flags *)(HEAP_START +
                              ((size_t)(ptr - ALLOC_START) / PAGE_SIZE));
 }
