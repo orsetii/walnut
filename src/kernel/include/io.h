8fe35fc0578c1ad7b09a878f0303aecef683ba76
Tom French <hello@orseti.com>
2024-02-22 11:39:56 +0000

    add `mtime` constant

--- src/kernel/include/io.h
+++ src/kernel/include/io.h
@@ -6,7 +6,10 @@
 // ===================== MMIO =======================
 // ==================================================
 
+#define mtimecmp 0x02004000
+#define mtime 0x0200bff8
+
 void mmio_writeb(addr_t addr, int offset, byte_t val);
 char mmio_readb(addr_t addr, int offset);
 
-#endif 
+#endif
